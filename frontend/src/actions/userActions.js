b2937f6a203e79a2755dc974c03fe52e2ddecd9f
kabbina <guhan.kabbina@gmail.com>
2020-12-04 00:02:55 +0530

    origin

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -60,6 +60,7 @@ export const register = (name, email, password) => async (dispatch) => {
     })
 
     const config = {
+      
       headers: {
         'Content-Type': 'application/json',
       },
